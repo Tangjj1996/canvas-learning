830e26060b069c9d4917ea9e1e591196f7787673
Tangjj1996 <Tangjj_1996@163.com>
2020-02-29 22:30:37 +0800

    add: drawRoundRect shape

--- canvas-paint/src/index.js
+++ canvas-paint/src/index.js
@@ -3,7 +3,20 @@ import shapes from './utils/shape'
 const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
 
-canvas.width = 800
-canvas.height = 800
+canvas.width = document.body.clientWidth 
+canvas.height = document.body.clientHeight - 4 
 
-shapes.drawStar(ctx, 100, 50, 400, 400)
+let skyStyle = ctx.createRadialGradient(canvas.width / 2, canvas.height, 0, canvas.width / 2, canvas.height, canvas.height)
+skyStyle.addColorStop(0.0, '#035')
+skyStyle.addColorStop(1.0, 'black')
+ctx.fillStyle = skyStyle
+ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+for(let i = 0; i < 200; i++) {
+    let r = Math.random() * 2 + 1
+    let x = Math.random() * canvas.width
+    let y = Math.random() * canvas.height * 0.5
+    let a = Math.random() * 360
+
+    shapes.drawStar(ctx, r, x, y, a)
+}
--- canvas-paint/src/utils/shape.js
+++ canvas-paint/src/utils/shape.js
@@ -1,17 +1,69 @@
-function drawStar(ctx, R, r, x, y, rotate = 0) {    
+/**
+ * 星星
+ * @param {canvas} ctx  画布
+ * @param {number} R 半径
+ * @param {number} x 圆心x
+ * @param {number} y 圆心y
+ * @param {number} rotate 旋转角度
+ */
+function drawStar(ctx, R,x, y, rotate = 0) {    
+    ctx.save()
+
+    ctx.translate(x, y)
+    ctx.rotate(rotate / 180 * Math.PI)
+    ctx.scale(R, R)
+    
+    starPath(ctx)
+    
+    ctx.fillStyle = '#fb3'
+
+    ctx.fill()
+    ctx.restore()
+}
+
+function starPath(ctx) {
     ctx.beginPath()
     for(let i = 0; i < 5; i++) {
-        ctx.lineTo(Math.cos((18 + i*72 - rotate)/180 * Math.PI) * R + x, 
-                -Math.sin((18 + i*72 - rotate)/180 * Math.PI) * R + y)
-        ctx.lineTo(Math.cos((54 + i*72 - rotate)/180 * Math.PI) * r + x, 
-                -Math.sin((54 + i*72 - rotate)/180 * Math.PI) * r + y)
+        ctx.lineTo(Math.cos((18 + i*72)/180 * Math.PI) * 10, 
+                -Math.sin((18 + i*72)/180 * Math.PI) * 10)
+        ctx.lineTo(Math.cos((54 + i*72)/180 * Math.PI) * 0.5 * 10, 
+                -Math.sin((54 + i*72)/180 * Math.PI) * 0.5 * 10)
     }
     ctx.closePath()
-    
-    ctx.lineWidth = 5
+}
+
+/**
+ * 圆角矩形
+ * @param {canvas} ctx 画布
+ * @param {number} x 坐标x
+ * @param {number} y 坐标y
+ * @param {number} width 宽度
+ * @param {number} height 高度
+ * @param {number} radius 圆角弧度半径
+ */
+
+function drawRoundRect(ctx, x, y, width, height, radius) {
+    ctx.save()
+    ctx.translate(x, y)
+    pathRouteRect(ctx, width, height, radius)
+    ctx.strokeStyle = '#fff'
     ctx.stroke()
+    ctx.restore()
+}
+
+function pathRouteRect(ctx, width, height, radius) {
+    ctx.beginPath()
+    ctx.arc(width - radius, height - radius,radius, 0, Math.PI / 2)
+    ctx.lineTo(radius, height)
+    ctx.arc(radius, height - radius, radius, Math.PI / 2, Math.PI)
+    ctx.lineTo(0, radius)
+    ctx.arc(radius, radius, radius, Math.PI, Math.PI * 3 / 2)
+    ctx.lineTo(width - radius, 0)
+    ctx.arc(width - radius, radius, radius, Math.PI * 3 / 2, 0)
+    ctx.closePath()
 }
 
 export default {
-    drawStar
+    drawStar,
+    drawRoundRect
 }
